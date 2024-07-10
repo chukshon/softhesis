78da7dc067a30aeb86c05603ae8c5fa9c3074041
ChukwukaHonour <ChukwukaHonour@gmail.com>
2024-07-10 07:42:16 +0100

    Added a check in the landing page that either navigates to the dashboard or login page if logged in or not

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,21 @@
+"use client";
+import { useUser } from "@/context/UserContext";
+import Link from "next/link";
+
 export default function Home() {
-  return <main className="">Home Page</main>;
+  const { user } = useUser();
+  return (
+    <main className="">
+      <h1 className="text-center text-[30px]">Welcome to Softhesis</h1>
+      {user ? (
+        <Link href="/dashboard" className="text-blue-700 text-center block">
+          Dashboard
+        </Link>
+      ) : (
+        <Link href="/login" className="text-blue-700 text-center block">
+          Kindly Login
+        </Link>
+      )}
+    </main>
+  );
 }
