f546743e3d6a6e80a6e41f6309d985dacefaf736
ChukwukaHonour <ChukwukaHonour@gmail.com>
2024-07-10 07:14:16 +0100

    Added a check to the profile page  to prevent non logged in users

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,9 +1,27 @@
+"use client";
+
+import UpdateProfileForm from "@/components/organisms/UpdateProfileForm";
+import { useUser } from "@/context/UserContext";
+import Link from "next/link";
 import React from "react";
 
 const ProfilePage = () => {
+  const { user } = useUser();
+
+  if (!user) {
+    return (
+      <div className="min-h-screen flex flex-col items-center justify-center">
+        <p>You need to be Logged in First.</p>
+        <Link href="login" className="text-blue-700">
+          Login
+        </Link>
+      </div>
+    );
+  }
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      ProfilePage
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <h1>Profile</h1>
+      <UpdateProfileForm />
     </div>
   );
 };
--- components/organisms/UpdateProfileForm.tsx
+++ components/organisms/UpdateProfileForm.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { profileSchema } from "@/schemas/userSchema";
 import React from "react";
 import { z } from "zod";
@@ -6,25 +8,37 @@ import { useUser } from "@/context/UserContext";
 import { useForm, SubmitHandler } from "react-hook-form";
 import FormField from "../molecules/FormField";
 import Button from "../atoms/Button";
+import { useRouter } from "next/navigation";
 
 type ProfileFormInputs = z.infer<typeof profileSchema>;
 
 const UpdateProfileForm = () => {
+  const { user, setUser } = useUser();
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<ProfileFormInputs>({
+    defaultValues: {
+      name: user?.name || "",
+      email: user?.email || "",
+      password: "",
+    },
     resolver: zodResolver(profileSchema),
   });
-  const { user, setUser } = useUser();
+
+  const router = useRouter();
   const onSubmit: SubmitHandler<ProfileFormInputs> = (data) => {
     setUser(data);
     alert("Profile updated successfully");
+    router.push("/dashboard");
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="max-w-md mx-auto mt-10">
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="max-w-[700px] w-full mx-auto mt-10"
+    >
       <FormField
         label="Name"
         name="name"
