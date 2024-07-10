e5162aca90ec9de5552a52c7fc31e156cb258e8e
ChukwukaHonour <ChukwukaHonour@gmail.com>
2024-07-10 21:02:25 +0100

    Restyled the input field

--- components/atoms/Input.tsx
+++ components/atoms/Input.tsx
@@ -15,7 +15,7 @@ const Input = ({ name, type, placeholder, register, required }: InputProps) => {
       type={type}
       placeholder={placeholder}
       {...register(name, { required })}
-      className="border p-2 w-full"
+      className="text-[#1C1C1C] text-regular-14 rounded-[8px] outline-none placeholder:text-[rgba(28,28,28,0.2)] border py-[8px] px-[16px] h-[40px] w-full focus:border-[#95A4FC] border-[rgba(28,28,28,0.1)]"
     />
   );
 };
