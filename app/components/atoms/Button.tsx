8124ff75c7241e9200f4b58aac00b4fbd80e35da
ChukwukaHonour <ChukwukaHonour@gmail.com>
2024-07-10 06:07:32 +0100

    Created the button component

---
+++ app/components/atoms/Button.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+interface ButtonProps {
+  type: "button" | "submit" | "reset";
+  onClick?: () => void;
+  children: React.ReactNode;
+}
+
+const Button = ({ type, onClick, children }: ButtonProps) => {
+  return (
+    <button
+      className="bg-blue-500 text-white py-2 px-4 rounded"
+      onClick={onClick}
+      type={type}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
