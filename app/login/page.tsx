9e250911961c3cbdbc1fe3b3c7c10ae71ae0b720
ChukwukaHonour <ChukwukaHonour@gmail.com>
2024-07-10 07:42:29 +0100

    Added the demo user info for quick login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -6,6 +6,10 @@ const LoginPage = () => {
     <div>
       <h1 className="text-center text-[30px] font-[600]">Login Page</h1>
       <LoginForm />
+      <span className="flex flex-col items-center j">
+        <p>UserName: user@example.com</p>
+        <p>Password: password</p>
+      </span>
     </div>
   );
 };
