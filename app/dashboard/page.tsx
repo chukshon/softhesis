f5411c09f0ba6fcfbfd85a968813c0be4d2a49a2
ChukwukaHonour <ChukwukaHonour@gmail.com>
2024-07-10 06:58:39 +0100

    Added a check in the dashboard page that prevents the user if not logged in

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,19 @@
+"use client";
+import { useUser } from "@/context/UserContext";
+import Link from "next/link";
 import React from "react";
 
 const DashboardPage = () => {
+  const { user } = useUser();
+
+  if (!user) {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <p>You need to be Logged in First.</p>
+        <Link href="login">Login</Link>
+      </div>
+    );
+  }
   return <div>Dashboard page test</div>;
 };
 
